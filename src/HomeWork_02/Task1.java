01dc28dd3a0fd73c87697b5d5446b7958aeeeca1
Korelov Maksim <korelov@gmail.com>
2021-08-29 11:41:15 +0300

    done HomeWork 2

---
+++ src/HomeWork_02/Task1.java
@@ -0,0 +1,14 @@
+package HomeWork_02;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 11, b = 20;
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println((double) a / b);
+        System.out.println(a % b);
+        System.out.println(a%2==0);
+        System.out.println(b%2==0);
+    }
+}
---
+++ src/HomeWork_02/Task2.java
@@ -0,0 +1,8 @@
+package HomeWork_02;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String smile = "\uD83D\uDE00";
+        System.out.println(smile);
+    }
+}
