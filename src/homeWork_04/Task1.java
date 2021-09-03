f9a7c04742970411cacd0e4ce4292e57e4c19fba
Korelov Maksim <korelov@gmail.com>
2021-09-03 19:48:51 +0300

    HomeWork 4 done 1/3

---
+++ src/homeWork_04/Task1.java
@@ -0,0 +1,15 @@
+package homeWork_04;
+/*
+Задача №1
+
+Необходимо вывести числа от 0 до 15.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a<=15) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
