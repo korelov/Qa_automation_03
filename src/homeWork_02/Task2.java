a92e6fa96e2fd31d0bc4c1d1c9eede4abbca0bad
Korelov Maksim <korelov@gmail.com>
2021-09-03 19:37:28 +0300

    HomeWork 2 update

--- src/homeWork_02/Task2.java
+++ src/homeWork_02/Task2.java
@@ -1,5 +1,9 @@
 package homeWork_02;
+/*
+Экстра задача
 
+Вывести/напечатать смайлик (не :), а настоящий смайлик одним символом).
+ */
 public class Task2 {
     public static void main(String[] args) {
         String smile = "\uD83D\uDE00";
