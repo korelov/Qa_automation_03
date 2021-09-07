ef03d964b10d423ce60f45ca47b76e5484fd805a
Korelov Maksim <korelov@gmail.com>
2021-09-07 12:05:28 +0300

    HomeWork 4 done 3/3   update 2 and 3 task

--- src/homeWork_04/Task2.java
+++ src/homeWork_04/Task2.java
@@ -7,10 +7,16 @@ package homeWork_04;
 public class Task2 {
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i < 1000) {
-            System.out.print(i +" ");
+        int i = 1;
+        while (i < 10000) {
+            System.out.println(i);
             i *= 5;
         }
+
+        for (int a = 1; a < 10000; a = a*5) {
+            System.out.println(a);
+        }
+
+
     }
 }
--- src/homeWork_04/Task3.java
+++ src/homeWork_04/Task3.java
@@ -16,6 +16,12 @@ public class Task3 {
                 System.out.println(i);
             }
         }
+        System.out.println("======================");
+        for (int a = 40 ;   a <=60 ; a = a + 4  ) {
+            //if (i%4==0) {
+                System.out.println(a);
+            }
+        }
 
     }
-}
+
