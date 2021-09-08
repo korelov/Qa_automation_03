8da6d5d4c315d170f497892ccdde23ef22c3ae76
Korelov Maksim <korelov@gmail.com>
2021-09-08 23:38:08 +0300

    HomeWork 4 new task loop 1

---
+++ src/homeWork_04/Task4.java
@@ -0,0 +1,30 @@
+package homeWork_04;
+/*
+Вывести следующие строки с соответствующим форматированием (как пирамиды):
+
+Задача №1
+
+0  1  2  3  4  5  6  7  8  9
+0  1  2  3  4  5  6  7  8
+0  1  2  3  4  5  6  7
+0  1  2  3  4  5  6
+0  1  2  3  4  5
+0  1  2  3  4
+0  1  2  3
+0  1  2
+0  1
+0
+ */
+public class Task4 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        while (a > 0) {
+            for (int i =0; i < a  ; i = i + 1 ) {
+                System.out.print(i + " ");
+            }
+              a=a-1;
+            System.out.print('\n');
+        }
+    }
+}
