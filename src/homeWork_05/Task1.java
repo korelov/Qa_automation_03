51f8e45043c7bae10e594087adec1e415f6ee963
Korelov Maksim <korelov@gmail.com>
2021-09-10 23:31:14 +0300

    HomeWork 5 1/6

---
+++ src/homeWork_05/Task1.java
@@ -0,0 +1,20 @@
+package homeWork_05;
+
+public class Task1 {
+    public static void main(String[] args) {
+        /*
+         Задача №1
+
+          Дан массив:
+          int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+          необходимо вывести сумму всех значений массива.
+             */
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        System.out.println(array[0]+array[1]+array[2]+array[3]+array[4]+array[5]+array[6]+array[7]+array[8]);
+
+
+
+
+
+    }
+}
