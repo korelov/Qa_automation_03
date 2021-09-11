46084e3c794b1b92b186acec1ea0f280169ce16f
Korelov Maksim <korelov@gmail.com>
2021-09-11 14:21:42 +0300

    HomeWork 5 6/6 максимальное значение вложенного массива

---
+++ src/homeWork_05/Task6.java
@@ -0,0 +1,26 @@
+package homeWork_05;
+
+import java.util.Arrays;
+
+/*
+Дан массив:
+int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+необходимо вывести максимальное значение массива.
+ */
+public class Task6 {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+
+        int max = 0;
+
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++)
+                if (max < array[i][j] ){
+                    max = array[i][j];
+                }
+        }
+        System.out.println("Длина массива = " + array.length );
+        System.out.println("Массив  " + Arrays.deepToString(array));
+        System.out.println("Максимальное значение массива = " + max);
+    }
+}
