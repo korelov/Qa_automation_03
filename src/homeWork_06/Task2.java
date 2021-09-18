ea2af8d981444dbaa1b295451d639763f2edf359
Korelov Maksim <korelov@gmail.com>
2021-09-18 19:01:15 +0300

    HomeWork6 2/3 количество заданных букв в строке

---
+++ src/homeWork_06/Task2.java
@@ -0,0 +1,20 @@
+package homeWork_06;
+/*
+Дана строка:
+String s = “Перевыборы выбранного президента”;
+необходимо подсчитать количество букв “е” в строке.
+Для указанной строки ответ будет 4.
+
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        String s = "Перевыборы выбранного президента";
+        int count = 0;
+        for (int i=0 ; i < s.length();i++){
+            if ('е' == s.charAt(i)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
