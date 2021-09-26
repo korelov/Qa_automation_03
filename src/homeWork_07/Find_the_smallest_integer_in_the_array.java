ec2ba84bf0b726027230f4efcca899ba626c17ec
Korelov Maksim <korelov@gmail.com>
2021-09-26 21:29:47 +0300

    Given an array of integers your solution should find the smallest integer.

---
+++ src/homeWork_07/Find_the_smallest_integer_in_the_array.java
@@ -0,0 +1,24 @@
+package homeWork_07;
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/java
+public class Find_the_smallest_integer_in_the_array {
+    public static int findSmallestInt(int[] args) {
+        int min = args[0];
+        for (int i=0; i < args.length; i++ ){
+            if ( min > args[i]){
+                min=args[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{78,56,232,12,11,43};
+        System.out.println(findSmallestInt(a));
+
+        int[] a1 = new int[]{78,56,-2,12,8,-33};
+        System.out.println(findSmallestInt(a1));
+
+        int[] a2 = new int[]{0,Integer.MIN_VALUE,Integer.MAX_VALUE};
+        System.out.println(findSmallestInt(a2));
+    }
+}
