96194a8dee61f13449a450587a5e1fd940f2e38b
Korelov Maksim <korelov@gmail.com>
2021-10-16 16:03:56 +0300

    Hw 13 OOP update

--- src/homeWork_13/Main.java
+++ src/homeWork_13/Main.java
@@ -6,22 +6,15 @@ public class Main {
 
         Worker worker1 = new Worker();
         worker1.setName("Ann");
-        worker1.setSalary(500);
+        worker1.setSalary(555);
         Worker worker2 = new Worker();
         worker2.setName("Mike");
         worker2.setSalary(550);
 
-        System.out.println("Количество рабочих " + Worker.count );
+        System.out.println("Количество рабочих " + Worker.count);
 
         worker1.showInfoEmployee();
         worker2.showInfoEmployee();
-
-        Worker[] arr1 = {worker1,worker2};
-
-        System.out.println(Utill.findName("Ann", arr1).getName());
-        System.out.println(Utill.sumSalary(arr1));
-
-
 
 
         //создаем менеджера
@@ -29,7 +22,7 @@ public class Main {
         man1.setBaseSalary(1);
         man1.setName("Maksim");
         man1.setSalary(1000);
-        man1.setNumberOfSubordinates(Worker.count-Manager.countManager);
+        man1.setNumberOfSubordinates(Worker.count - Manager.countManager);
 
         //Manager man2 = new Manager();
 
@@ -41,7 +34,6 @@ public class Main {
         System.out.println("=======");
 
 
-
         //создаем директора
         Director dir = new Director();
         dir.setName("Vasia");
@@ -50,11 +42,22 @@ public class Main {
         dir.showInfoEmployee();
 
 
-
         //общее количество сотрудников включая рабочих, менеджера и директора
         System.out.println("общее количество сотрудников " + Worker.count);
 
+        Worker[] arr1 = {worker1, worker2, man1, dir};
 
+
+        //System.out.println(Utill.findName("Ann", arr1).getName());
+        System.out.println(Utill.sumSalary(arr1));
+
+
+        //создаем массив из рабочих , менеджеров и директора
+        Employee[] arr2 = {worker1, worker2, man1, dir};
+
+
+        System.out.println(Utill.minSalary(arr2).getSalary());
+        System.out.println(Utill.minSalary(arr2).getName());
 
     }
 
