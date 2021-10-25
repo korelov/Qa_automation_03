14de5719a344a6f704de96fcef172d7cdbbfe0c9
Korelov Maksim <korelov@gmail.com>
2021-10-25 20:00:44 +0300

    HW 15 Task  1/3

---
+++ src/homeWork_15/MonthUtils.java
@@ -0,0 +1,17 @@
+package homeWork_15;
+
+public class MonthUtils {
+
+    static Month january = new Month("January ", 31, 15);
+    public static Month february = new Month("February  ", 28, 19);
+    public static Month march = new Month("March ", 31, 22);
+    public static Month april = new Month("April  ", 30, 22);
+    public static Month may = new Month("May  ", 31, 15);
+    public static Month june = new Month("June  ", 30, 21);
+    public static Month july = new Month("July  ", 31, 22);
+    public static Month august = new Month("August  ", 31, 22);
+    public static Month september = new Month("September  ", 30, 22);
+    public static Month october = new Month("October  ", 31, 21);
+    public static Month november = new Month("November  ", 30, 17);
+    public static Month december = new Month("December  ", 31, 22);
+}
