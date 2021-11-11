3a1aed93d5945f8eb73b0095ff4aa95085937d35
Korelov Maksim <korelov@gmail.com>
2021-11-11 20:00:55 +0300

    HW 17 all done

---
+++ src/homeWork_17/Manager.java
@@ -0,0 +1,32 @@
+package homeWork_17;
+
+//Классы Employee и Manager должны быть финальными
+
+public final class Manager extends BaseEmployee {
+
+    int numberOfSubordinates;
+
+    public Manager(String name, int age, char gender, int salaryDay) {
+        super(name, age, gender, salaryDay);
+    }
+
+    public int getNumberOfSubordinates() {
+        return numberOfSubordinates;
+    }
+
+    public void setNumberOfSubordinates(int numberOfSubordinates) {
+        this.numberOfSubordinates = numberOfSubordinates;
+    }
+
+    @Override
+    public int getSalary(Month[] monthArray) {
+        System.out.println("Зп за выбранные месяца или квартал. ");
+        return super.getSalary(monthArray) + (super.getSalary(monthArray) / 100 * numberOfSubordinates);
+    }
+
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println("Количество подчиненных " + numberOfSubordinates);
+    }
+}
