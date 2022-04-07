8ddb78e4998a32b3d1ec42085b738c2e77c0d167
Korelov Maksim <korelov@gmail.com>
2022-04-07 15:19:43 +0300

    HW 6 QA_4

---
+++ src/homeWork_06/Playing_with_cubes.java
@@ -0,0 +1,24 @@
+package homeWork_06;
+// https://www.codewars.com/kata/55c0a79e20be94c91400014b/java
+
+ class Cube {
+
+    int Side;
+
+    public int getSide() {
+        return Side;
+    }
+
+    public void setSide(int side) {
+        Side = side;
+    }
+}
+
+public class Playing_with_cubes {
+
+    public static void main(String[] args) {
+        Cube cube = new Cube();
+        cube.setSide(5);
+        System.out.println(cube.getSide());
+    }
+}
